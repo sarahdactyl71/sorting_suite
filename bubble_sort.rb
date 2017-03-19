6a1d72af500a99a345857ccbee8259f106f72eee
Sarah Kirk <kirk710@gmail.com>
2017-03-19 16:59:47 -0600

    uploads file

---
+++ bubble_sort.rb
@@ -0,0 +1,32 @@
+#Bubble Sort Test
+
+class BubbleSort
+    def initialize (array)
+        @array = array
+        switch = 0
+    end
+
+    def sort_this
+        x = @array.length
+
+      @array.each do
+         switch = 0
+         @array.map_with_index do |character, index|
+             break if index == (x-1)
+            if character > @array[index + 1]
+                 @array[index], @array[index +1] = @array[index +1], @array[index]
+                 switch += 1
+            end
+          end
+          break if switch == 0
+        end
+    @array
+    end
+
+end
+
+number_sorter = BubbleSort.new([7,5,2,3,8,11,24,6,15,13])
+puts number_sorter.sort_this
+
+letter_sorter = BubbleSort.new(["z","r","l","a","f","x","a"])
+puts letter_sorter.sort_this
